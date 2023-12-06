f1356b03d9da2d81b4030f07786bccdca1ebf49c
Bardia <Bardiaa830@gmail.com>
2023-12-06 16:22:11 +0330

    Finilize2

--- Width Changer/tmp.js
+++ Width Changer/tmp.js
@@ -1,11 +1,36 @@
 let Idout;
 let Idin;
-
+let unit = 2
+let width;
 function increase(){
+    width = document.getElementById('img').width
+    clearInterval(Idin);
     Idout = setInterval(expand, 20);
 }
 
 function expand(){
-    img = document.getElementById('img');
-    width = img
+    let image = document.getElementById('img');
+    if(width < 650){
+        width = width + unit;
+        image.width = width;
+    }
+    else{
+        clearInterval(Idout);
+    }
+}
+
+function decrease(){
+    clearInterval(Idout);
+    Idin = setInterval(shrink, 20);
+}
+
+function shrink(){
+    let imag = document.getElementById('img');
+    if(width > 400){
+        width = width - unit;
+        imag.width = width
+    }
+    else{
+        clearInterval(Idin);
+    }
 }
