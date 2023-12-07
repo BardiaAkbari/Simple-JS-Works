1b1f587eddd07f670f5a6baecb61e79d1a1f4cd2
Bardia <Bardiaa830@gmail.com>
2023-12-07 18:15:43 +0330

    finalize9

---
+++ HeartRain/tmp.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+    function createHeart(){
+        const heart = document.createElement('div');
+        heart.style.left = Math.random() * 100 + 'vw';
+        heart.style.animationDuration = Math.random() * 2 + 3 +'s';
+        heart.classList.add('heart');
+        heart.innerText = '❤️';
+        document.body.appendChild(heart);
+        setTimeout(function(){
+            heart.remove()
+        }, 5000)
+    }
+    setInterval(createHeart, 10);
+}
